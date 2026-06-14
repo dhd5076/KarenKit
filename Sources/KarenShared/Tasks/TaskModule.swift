f2d77f464d1064b809b10c3ecdd5361298f80d0e
Dylan Dunn <dylandunn17@icloud.com>
2026-06-14 15:24:27 -0400

    Added TaskModule

---
+++ Sources/KarenShared/Tasks/TaskModule.swift
@@ -0,0 +1,12 @@
+//
+//  TaskModule.swift
+//  KarenShared
+//
+//  Created by Dylan Dunn on 6/14/26.
+//
+
+public struct TaskModule: KarenModule {
+    public static let route = "task"
+    public static let displayName = "Tasks"
+    public static let icon = "checklist"
+}
